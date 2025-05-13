562313774bd301b395d3f0aefb64911c0263f4a9
Namnika <janbandhunamnika@gmail.com>
2025-05-13 15:52:46 +0530

    remove email and phone number pattern for login

--- BackendApplication/src/main/java/com/backend/BackendApplication/dto/UserLoginDto.java
+++ BackendApplication/src/main/java/com/backend/BackendApplication/dto/UserLoginDto.java
@@ -1,8 +1,6 @@
 package com.backend.BackendApplication.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,10 +9,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserLoginDto {
-    @NotBlank(message = "Email cannot be empty")
-    @Email(message = "Invalid email format")
-    @NotBlank(message = "Phone number cannot be empty")
-    @Pattern(regexp = "^(\\+\\d{1,3}[- ]?)?\\d{10}$", message = "Invalid phone number format")
+
+    @NotBlank(message = "Email or phone number cannot be empty")
     private String emailOrPhone; // either checks phone number or email for login
 
     @NotBlank(message = "Password cannot be empty")
