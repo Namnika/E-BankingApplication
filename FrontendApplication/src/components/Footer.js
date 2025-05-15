3d1f433552b825992b1dc7884edcc1a81a35bfc0
Namnika <janbandhunamnika@gmail.com>
2025-05-15 19:30:02 +0530

    Add Footer with copyright details

---
+++ FrontendApplication/src/components/Footer.js
@@ -0,0 +1,16 @@
+
+
+const Footer = () => {
+
+    const currentYear = new Date().getFullYear();
+    const brandName = "NexusBank";
+
+    return (
+        <footer className="footer">
+            <p>&copy; {currentYear} {brandName}. All Rights Reserved.</p>
+            <p>This project is associated to MCA Final Year Capstone Project in Jain (Deemed-to-be-University). Made By Namnika Janbandhu</p>
+        </footer>
+    )
+}
+
+export default Footer
