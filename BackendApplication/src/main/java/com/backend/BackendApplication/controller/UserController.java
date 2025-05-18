0765b526e98d3817bc73a396a096abb319bcda49
Namnika <janbandhunamnika@gmail.com>
2025-05-18 23:11:42 +0530

    modify login admin unauthorized response

--- BackendApplication/src/main/java/com/backend/BackendApplication/controller/UserController.java
+++ BackendApplication/src/main/java/com/backend/BackendApplication/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.backend.BackendApplication.dto.LoginResponseDto;
 import com.backend.BackendApplication.dto.UserLoginDto;
@@ -41,8 +43,12 @@ public class UserController {
     // Login user
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody UserLoginDto dto) {
-        LoginResponseDto response = userService.loginUser(dto);
-        return ResponseEntity.ok(response);
+        try {
+            LoginResponseDto response = userService.loginUser(dto);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        }
     }
 
     // Get user by ID
