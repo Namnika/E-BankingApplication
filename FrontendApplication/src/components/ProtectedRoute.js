ad069d982def87307509d6d93ca1ab1dedf0666d
Namnika <janbandhunamnika@gmail.com>
2025-05-18 23:16:20 +0530

    add requireRole props for admin priviledges in protectedRoute

--- FrontendApplication/src/components/ProtectedRoute.js
+++ FrontendApplication/src/components/ProtectedRoute.js
@@ -3,12 +3,17 @@ import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = ({ children, requiredRole }) => {
     const { auth } = useContext(AuthContext);
 
     if (!auth || !auth.token) {
         // Redirect to login if not authenticated
         return <Navigate to="/login" replace />;
+    }
+
+    // If role is required and user doesn't have it, redirect to home
+    if (requiredRole && auth.user.role !== requiredRole) {
+        return <Navigate to="/" />;
     }
 
     return children;
